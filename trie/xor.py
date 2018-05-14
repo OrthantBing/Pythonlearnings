ac8a10c1c92fadf2940452b346c58dfa6e3fbbe0
Anton Bright Thomson <antonbright.t@aggulabs.com>
2018-05-14 13:32:21 +0530

    xor subarray with index

--- trie/xor.py
+++ trie/xor.py
@@ -1,117 +1,214 @@
-#https://www.hackerearth.com/practice/notes/lalitkundu95/tutorial-on-trie-and-example-problems/
 import sys
-MAX_INT_LEN = len(bin(sys.maxint)[2:])
+MAX_LEN_INT = len(bin(sys.maxint)[2:])
 
 def NumStringToBinString(string):
     num = int(string)
-    return bin(num)[2:].zfill(MAX_INT_LEN)
+    return bin(num)[2:].zfill(MAX_LEN_INT)
 
 def BinStringToNumString(string):
     return int(string, 2)
 
-
-
 class TrieNode(object):
-    __slots__ = 'element','value','word_finished','zero', 'one','counter'
+    __slots__ = 'element', 'value', 'index', 'counter', 'zero', 'one', 'word_finished'
 
     def __init__(self, element, value=None):
         self.element = element
         self.value = value
         self.word_finished = False
         self.counter = 0
-        # self.children = []
         self.zero = None
         self.one = None
+        self.index = None
 
-    def add(self, root, string):
+    def add(self, root, string, arrindex):
         node = root
+
         for index, char in enumerate(string):
-            child_found = False
-            
-            if node.zero is not None: 
-                if node.zero.element == char:
-                    node.zero.counter += 1
-                    node = node.zero
-                    child_found = True
-            elif node.one is not None:
-                if node.one.element == char:
-                    node.one.counter += 1
+            if char == '1':
+                if node.one is not None:
                     node = node.one
-                    child_found = True
-            
-            # for child in node.children:
-            #     if child.element == char:
-            #         child.counter += 1
-            #         node = child
-            #         child_found = true
+                    node.counter += 1
 
-            if not child_found:
-                new_node = TrieNode(char)
-                new_node.value = string[:index+1]
-                # node.children.append(new_node)
-                if char == '1':
-                    node.one = new_node
+                    
                 else:
+                    new_node = TrieNode(char)
+                    new_node.value = string[:index+1]
+                    node.one = new_node
+                    node = new_node
+            elif char == '0':
+                if node.zero is not None:
+                    node = node.zero
+                    node.counter += 1
+
+                   
+                else:
+                    new_node = TrieNode(char)
+                    new_node.value = string[:index + 1]
                     node.zero = new_node
-
-                node = new_node
-
+                    node = new_node
+            else:
+                print "Something terribly wront happened"
+                exit()
+        
         node.word_finished = True
-
+        node.index = arrindex
 
     def query(self, root, string):
         node = root
-        finalstring = ''
-        for char in string:
+
+        for index, char in enumerate(string):
             if char == '1':
                 if node.zero is not None:
-                    finalstring += node.zero.element
                     node = node.zero
-                elif node.one is not None:
-                    finalstring += node.one.element
+                else: 
                     node = node.one
-                else:
-                    print "Something terribly wrong"
-                    exit()
-            else:
+            elif char == '0':
                 if node.one is not None:
-                    finalstring += node.one.element
                     node = node.one
-                elif node.zero is not None:
-                    finalstring += node.zero.element
-                    node = node.zero
                 else:
-                    print "Something terribly wrong"
-                    exit()
-        return finalstring
+                    node = node.zero
+            else:
+                print "something terribly wrong"
+                exit()
+                
+        return int(BinStringToNumString(string)) ^ int(BinStringToNumString(node.value)), node.index
 
 
-    def max_xor_subarray(self, root, array):
+    def xor_sub_array(self, root, array):
         ans = 0
         pre = 0
-        # root.add(root, "0")
+
+        initial = 0
+        final = 0
         for i in xrange(len(array)):
             pre = pre ^ int(array[i])
-            root.add(root, NumStringToBinString(str(pre)))
+            root.add(root, NumStringToBinString(str(pre)), i)
             s = NumStringToBinString(str(pre))
-            ans = max(ans, int(BinStringToNumString(root.query(root, s))))
-            
-        print ans
+            print "String" + s
 
+            uptonow, index = root.query(root, s)
+
+            if ans < uptonow:
+                initial = index
+                ans = uptonow
+                final = i
+        
+        print "Max xor subarray:  " + str(ans)
+        print "initial: " + str(initial) 
+        print "Final: " + str(final)
 
 if __name__ == "__main__":
+
     string_arr = raw_input().split()
-    
-    root = TrieNode("*")
-    root.max_xor_subarray(root, string_arr)
-    # for string in string_arr:
-    #     root.add(root,NumStringToBinString(string))
+    root = TrieNode('*')
 
-    # inputcount = int(raw_input())
-
-    # for _ in xrange(inputcount):
-    #     s = NumStringToBinString(raw_input().strip())
-    #     print BinStringToNumString(root.query(root, s))
+    root.xor_sub_array(root, string_arr)
 
 
-     
+import sys
+MAX_LEN_INT = len(bin(sys.maxint)[2:])
+
+def NumStringToBinString(string):
+    num = int(string)
+    return bin(num)[2:].zfill(MAX_LEN_INT)
+
+def BinStringToNumString(string):
+    return int(string, 2)
+
+class TrieNode(object):
+    __slots__ = 'element', 'value', 'index', 'counter', 'zero', 'one', 'word_finished'
+
+    def __init__(self, element, value=None):
+        self.element = element
+        self.value = value
+        self.word_finished = False
+        self.counter = 0
+        self.zero = None
+        self.one = None
+        self.index = None
+
+    def add(self, root, string, arrindex):
+        node = root
+
+        for index, char in enumerate(string):
+            if char == '1':
+                if node.one is not None:
+                    node = node.one
+                    node.counter += 1
+
+                    
+                else:
+                    new_node = TrieNode(char)
+                    new_node.value = string[:index+1]
+                    node.one = new_node
+                    node = new_node
+            elif char == '0':
+                if node.zero is not None:
+                    node = node.zero
+                    node.counter += 1
+
+                   
+                else:
+                    new_node = TrieNode(char)
+                    new_node.value = string[:index + 1]
+                    node.zero = new_node
+                    node = new_node
+            else:
+                print "Something terribly wront happened"
+                exit()
+        
+        node.word_finished = True
+        node.index = arrindex
+
+    def query(self, root, string):
+        node = root
+
+        for index, char in enumerate(string):
+            if char == '1':
+                if node.zero is not None:
+                    node = node.zero
+                else: 
+                    node = node.one
+            elif char == '0':
+                if node.one is not None:
+                    node = node.one
+                else:
+                    node = node.zero
+            else:
+                print "something terribly wrong"
+                exit()
+                
+        return int(BinStringToNumString(string)) ^ int(BinStringToNumString(node.value)), node.index
+
+
+    def xor_sub_array(self, root, array):
+        ans = 0
+        pre = 0
+
+        initial = 0
+        final = 0
+        for i in xrange(len(array)):
+            pre = pre ^ int(array[i])
+            root.add(root, NumStringToBinString(str(pre)), i)
+            s = NumStringToBinString(str(pre))
+            print "String" + s
+
+            uptonow, index = root.query(root, s)
+
+            if ans < uptonow:
+                initial = index
+                ans = uptonow
+                final = i
+        
+        print "Max xor subarray:  " + str(ans)
+        print "initial: " + str(initial) 
+        print "Final: " + str(final)
+
+if __name__ == "__main__":
+
+    string_arr = raw_input().split()
+    root = TrieNode('*')
+
+    root.xor_sub_array(root, string_arr)
+
+
